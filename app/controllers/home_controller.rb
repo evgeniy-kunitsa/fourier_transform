6730b68fa6f27bb9e21c52be0b145ee23e213605
Victor Makoed <vmakoed@gmail.com>
2016-02-02 23:04:55 +0300

    1: Add Transformation Data Charts
    * Apply bootstrap
    * Convert transformation data to result hashes
    * Display transformation data with tables and charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,18 @@ class HomeController < ApplicationController
   def index
     manager = Fourier::DiscreteFourierTransformManager.new
     @direct_conversion = manager.direct_conversion
-    @reverse_conversion = manager.reverse_conversion 
+    @reverse_conversion = manager.reverse_conversion
+
+    @direct_conversion_result = {}
+    @direct_conversion_result[:indexes] = (0..@direct_conversion.size - 1).collect {|i| i}
+    @direct_conversion_result[:values] = @direct_conversion.collect do |conversion_unit|
+      conversion_unit[1][:value][:abs]
+    end
+
+    @reverse_conversion_result = {}
+    @reverse_conversion_result[:indexes] = (0..@reverse_conversion.size - 1).collect {|i| i}
+    @reverse_conversion_result[:values] = @reverse_conversion.collect do |conversion_unit|
+      conversion_unit[1][:value][:abs]
+    end
   end
 end
